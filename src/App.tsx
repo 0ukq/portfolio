8dde42ab053bcf1c4315f324dce140c230834831
平野 大騎 <daiki.hirano@caters.co.jp>
2025-12-05 15:49:18 +0900

    update react → next.js

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import AppRouter from './router/app-router';
-
-function App() {
-  return <AppRouter />;
-}
-
-export default App;
--- src/pages/page.tsx
+++
@@ -1,5 +0,0 @@
-import FirstView from '../components/Index/FirstView';
-
-export default function Home() {
-  return <FirstView />;
-}

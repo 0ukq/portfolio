c7d881ab1b1da3dd6576cb672f250196afe653fc
平野 大騎 <daiki.hirano@caters.co.jp>
2025-12-09 15:41:54 +0900

    update lenis gsap統合

--- src/components/Lenis/Lenis.tsx
+++ src/components/Lenis/Lenis.tsx
@@ -1,7 +1,20 @@
 'use client';
 
-import { ReactLenis } from 'lenis/react';
+import gsap from 'gsap';
+import { useEffect, useRef } from 'react';
+import { LenisRef, ReactLenis } from 'lenis/react';
 
 export default function Lenis() {
-  return <ReactLenis root />;
+  const lenisRef = useRef<LenisRef>(null);
+
+  useEffect(() => {
+    const update = (time: number) => {
+      lenisRef.current?.lenis?.raf(time * 1000);
+    };
+    gsap.ticker.add(update);
+
+    return () => gsap.ticker.remove(update);
+  }, []);
+
+  return <ReactLenis root options={{ autoRaf: false }} ref={lenisRef} />;
 }

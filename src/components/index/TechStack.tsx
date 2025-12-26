2851dea22c51db57de9bf8e5941fa2ebb1416dd7
平野 大騎 <daiki.hirano@caters.co.jp>
2025-12-26 18:50:10 +0900

    update ロゴアニメーション途中

--- src/components/index/TechStack.tsx
+++ src/components/index/TechStack.tsx
@@ -7,20 +7,26 @@ import NextVector from '../vector/NextVector';
 import TsVector from '../vector/TsVector';
 import CssVector from '../vector/CssVector';
 import HtmlVector from '../vector/HtmlVector';
-import StackItems from '../stack/StackItems';
 import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
 import { SplitText } from 'gsap/SplitText';
+import DrawSVGPlugin from 'gsap/DrawSVGPlugin';
+import ScrollTrigger from 'gsap/ScrollTrigger';
 import { useEffect, useRef } from 'react';
 import RollingTextAnimate from '../animation/RollingTextAnimate';
-
-import styles from './TechStack.module.css';
 import HideUpAnimate from '../animation/HideUpAnimate';
 import clsx from 'clsx';
 
-gsap.registerPlugin(SplitText);
+import styles from './TechStack.module.css';
+import MotionPathPlugin from 'gsap/MotionPathPlugin';
+import MorphSVGPlugin from 'gsap/MorphSVGPlugin';
+import { easeOutCubic } from '@/lib/custom-ease';
+
+gsap.registerPlugin(SplitText, DrawSVGPlugin, MotionPathPlugin, MorphSVGPlugin, ScrollTrigger);
 
 const TechStack: React.FC = () => {
   const gsapRef = useRef<HTMLElement>(null);
+  const tl = useRef<gsap.core.Timeline | null>(null);
   const lottieRef = useRef<LottieRef>(null);
 
   useEffect(() => {
@@ -29,8 +35,65 @@ const TechStack: React.FC = () => {
     }
   }, []);
 
+  useGSAP(
+    () => {
+      if (gsapRef.current) {
+        // React Vector Animation
+        const reactPaths = gsapRef.current.querySelectorAll('[data-react-vector] path');
+        const nextPaths = gsapRef.current.querySelectorAll('[data-next-vector] path');
+        const tsPaths = gsapRef.current.querySelectorAll('[data-ts-vector] path');
+
+        // 共通の初期値
+        const setVars: gsap.TweenVars = {
+          drawSVG: '0%',
+          fill: 'none',
+        };
+
+        // drawSVG
+        const drawVars: gsap.TweenVars = {
+          drawSVG: '100%',
+          duration: 2,
+          ease: easeOutCubic,
+        };
+
+        // fill
+        const fillVars: gsap.TweenVars = {
+          duration: 1,
+          ease: easeOutCubic,
+        };
+
+        // 初期値設定
+        gsap.set(reactPaths, { ...setVars });
+        gsap.set(nextPaths, { ...setVars });
+        gsap.set(tsPaths, { ...setVars });
+
+        // tl設定
+        tl.current = gsap.timeline({
+          scrollTrigger: {
+            trigger: '[data-tech-stack-logos]',
+            start: 'top bottom-=30%',
+            markers: true,
+          },
+        });
+
+        console.log(tsPaths[0].getAttribute('stroke'));
+
+        tl.current
+          .to(reactPaths, { ...drawVars })
+          .to(nextPaths, { ...drawVars }, '<')
+          .to(tsPaths, { ...drawVars }, '<')
+          .to(reactPaths, { fill: '#61DAFB', ...fillVars }, '>')
+          .to(nextPaths, { fill: 'white', ...fillVars }, '<')
+          .to(tsPaths[0], { fill: '#3178C6', ...fillVars }, '<')
+          .to(tsPaths[1], { fill: '#3178C6', ...fillVars }, '<')
+          .to(tsPaths[2], { fill: 'white', ...fillVars }, '<');
+      }
+    },
+    { scope: gsapRef }
+  );
+
   return (
-    <section ref={gsapRef} className={styles.techStack}>
+    <section ref={gsapRef} className={clsx(styles.techStack, 'bg-main-gray')}>
       <ContentInner>
         <hgroup className={styles.heading}>
           <div className={clsx(styles.lottie, 'hover-lottie')}>
@@ -42,7 +105,7 @@ const TechStack: React.FC = () => {
             <RollingTextAnimate text="TECH STACK" />
           </HeadingText>
         </hgroup>
-        <ul className={styles.logos}>
+        <ul className={styles.logos} data-tech-stack-logos>
           <li>
             <ReactVector />
             <p>3YEAR.</p>

6bac399260a54e4133833fe3d495723260d529b8
平野 大騎 <daiki.hirano@caters.co.jp>
2026-01-05 16:32:13 +0900

    update hover アニメーション追加

--- src/components/card/WorkCard.tsx
+++ src/components/card/WorkCard.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link';
 import HideUpAnimate from '../animation/HideUpAnimate';
 
 import styles from './WorkCard.module.css';
+import HeadingText, { HeadingTextAlign, HeadingTextVariant } from '../heading/HeadingText';
 
 interface WorkCardProps {
   data: WorkData;
@@ -15,16 +16,28 @@ const WorkCard: React.FC<WorkCardProps> = ({ data }) => {
     <>
       {data && (
         <>
-          <Link href={`/work/${data.id}`} className={styles.link}>
-            <figure className={clsx(styles.thumbnail, 'clip')}>
-              <Image
-                src={data.thumbnail.src}
-                alt={data.title}
-                width={data.thumbnail.width}
-                height={data.thumbnail.height}
-                data-work-card-image
-              />
-            </figure>
+          <Link
+            href={`/work/${data.id}`}
+            className={clsx(styles.link, styles['link--hover-animation'])}
+          >
+            <div className={styles.thumbnail}>
+              <figure className={clsx(styles.image, 'clip')}>
+                <Image
+                  src={data.thumbnail.src}
+                  alt={data.title}
+                  width={data.thumbnail.width}
+                  height={data.thumbnail.height}
+                  data-work-card-image
+                />
+              </figure>
+              <HeadingText
+                align={HeadingTextAlign.CENTER}
+                variant={HeadingTextVariant.HEADING3}
+                className={styles.title}
+              >
+                {data.title}
+              </HeadingText>
+            </div>
             <div className={styles.description}>
               <HideUpAnimate start={'top bottom-=5%'}>
                 <p>{data.title}</p>

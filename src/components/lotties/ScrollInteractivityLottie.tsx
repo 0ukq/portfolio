3458a703a8e25b38ab0d8ead41978adeeb8a7721
hirano <daiki.hirano@caters.co.jp>
2026-04-16 17:53:57 +0900

    feat スクロール連動lottieコンポーネント作成

---
+++ src/components/lotties/ScrollInteractivityLottie.tsx
@@ -0,0 +1,29 @@
+import { useLottie, useLottieInteractivity } from 'lottie-react';
+
+interface ScrollInteractivityLottieProps {
+  lottieData: unknown;
+}
+
+const ScrollInteractivityLottie: React.FC<ScrollInteractivityLottieProps> = ({ lottieData }) => {
+  const options = {
+    animationData: lottieData,
+  };
+
+  const lottieObj = useLottie(options);
+
+  const Animation = useLottieInteractivity({
+    lottieObj,
+    mode: 'scroll',
+    actions: [
+      {
+        type: 'seek',
+        visibility: [0.2, 1],
+        frames: [0, 60],
+      },
+    ],
+  });
+
+  return Animation;
+};
+
+export default ScrollInteractivityLottie;
